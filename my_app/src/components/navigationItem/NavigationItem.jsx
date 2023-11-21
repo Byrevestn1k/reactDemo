3cd11f33fbb07a7bc300c6af8faf754db7098404
DESIGNER <designer@step.rivne>
2023-11-21 18:16:05 +0200

    repear letter

--- my_app/src/components/navigationItem/NavigationItem.jsx
+++ my_app/src/components/navigationItem/NavigationItem.jsx
@@ -1,22 +1,22 @@
-import './NavigationItem.css';
+import './navigationigit fetchtem.css';
 
 const NavigationItem = (props) => {
    const { text, isUppercasetext, description } = props
 
    const descriptionSection = (
-         <div className='description'>
-             { description }
-         </div>
+      <div className='description'>
+         {description}
+      </div>
    );
-  
+
    return (
       <div className='common'>
          <div className='text'>
-            { isUppercasetext ? text.toUpperCase() : text }
+            {isUppercasetext ? text.toUpperCase() : text}
          </div>
-         {descriptionSection && descriptionSection }
+         {descriptionSection && descriptionSection}
       </div>
-     )
+   )
 }
 
 export default NavigationItem;
